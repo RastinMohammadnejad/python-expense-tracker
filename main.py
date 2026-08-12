1db60544840e6545ee237990e57a8bbac287b6dd
Rastin <rasin.m.n82@gmail.com>
2026-08-12 21:26:42 +0330

    feat: add main menu

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+def initial_display():
+    user_option = input("""
+==============================
+    Python Expense Tracker
+=============================
+    1. Add Expense
+    2. View Expenses
+    3. Delete Expense
+    4. Show Total
+    5. Exit
+==============================
+    Choose an option:""")
+    return user_option
+    
 def main():
-    print("Python Expense Tracker")
-
-
+    is_on = True
+    while is_on:
+        user_answer = initial_display()
+        print(f"You selected: {user_answer}")
+    
+     
 if __name__ == "__main__":
     main()
