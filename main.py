0f09e8bebe1029ea5a97a035953313fcb182f2a6
Rastin <rasin.m.n82@gmail.com>
2026-08-12 22:07:23 +0330

    feat: add expense saving to JSON

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 def initial_display():
     user_option = input("""
 ==============================
@@ -11,6 +13,35 @@ def initial_display():
 ==============================
     Choose an option:""")
     return user_option
+
+
+
+def add_expense():
+    amount = input("Amount: ")
+    category = input("Category: ")
+    description = input("Description: ")
+    date = input("Date: ")
+
+    expense = {
+        "amount": amount,
+        "category": category,
+        "description": description,
+        "date": date
+    }
+
+    try:
+        with open("expenses.json", "r") as file:
+            expenses = json.load(file)
+    except FileNotFoundError:
+        expenses = []
+
+    expenses.append(expense)
+
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=4)
+
+    print("Expense saved successfully!")
+
     
 def main():
     is_on = True
@@ -19,7 +50,7 @@ def main():
         user_answer = initial_display()
         
         if user_answer == "1":
-            print("Add Expense")
+            add_expense()
         elif user_answer == "2":
             print("View Expenses")
         elif user_answer == "3":
